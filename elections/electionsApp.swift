49b36c5d666d212ebce176867e1754055a8c9237
Mendez <conarov@gmail.com>
2026-02-18 02:45:55 +0200

    Initial Commit

---
+++ elections/electionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  electionsApp.swift
+//  elections
+//
+//  Created by Mendez on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct electionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ electionsTests/electionsTests.swift
@@ -0,0 +1,17 @@
+//
+//  electionsTests.swift
+//  electionsTests
+//
+//  Created by Mendez on 2/18/26.
+//
+
+import Testing
+@testable import elections
+
+struct electionsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
